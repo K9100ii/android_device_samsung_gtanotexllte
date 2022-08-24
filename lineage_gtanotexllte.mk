2f7ee8519848e6f6eabc5d1732e13268389545df
Tim Zimmermann <tim@linux4.de>
2022-08-24 15:40:22 +0200

    gtanotexllte: Inherit non_ab_device.mk
    This is required to build applypatch for updating recovery.
    
    Change-Id: I38053691f7724b5d6c30159c410d5d04ac136180

--- lineage_gtanotexllte.mk
+++ lineage_gtanotexllte.mk
@@ -20,6 +20,7 @@ $(call inherit-product, device/samsung/gtanotexllte/device.mk)
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/non_ab_device.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_m.mk)
 
 # Inherit some common Lineage stuff.
