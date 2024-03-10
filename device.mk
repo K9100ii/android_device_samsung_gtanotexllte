1fdeccbe9edbdec39e428144aac97061cf910676
K9100ii <k9100ii.ki@gmail.com>
2024-03-10 19:12:41 +0000

    gtanotexllte: Remove android.hardware.telephony.ims permission
    No working IMS implementation is available, let alone in use.
    
    No seeming effect from this removal, so this is just for correctness.
    
    Change-Id: I29940de8fde163b2345aa9bb529cf30ad3f28ba3

--- device.mk
+++ device.mk
@@ -36,8 +36,7 @@ DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.sip.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.sip.xml \
     frameworks/native/data/etc/android.software.sip.voip.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.sip.voip.xml \
-    frameworks/native/data/etc/android.hardware.telephony.gsm.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.telephony.gsm.xml \
-    frameworks/native/data/etc/android.hardware.telephony.ims.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.telephony.ims.xml
+    frameworks/native/data/etc/android.hardware.telephony.gsm.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.telephony.gsm.xml
 
 # Ramdisk
 PRODUCT_PACKAGES += \
